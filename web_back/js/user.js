433dac535ea7b7089211e5f24c0a8b8a8e536df3
martin <895524036@qq.com>
2019-11-19 22:56:32 +0800

    页面加载退出完成

--- web_back/js/user.js
+++ web_back/js/user.js
@@ -1,11 +1,25 @@
+var url = 'http://localhost:8000/'
+
 var user = {
     login: function(myname, mypassword, callback) {
-        $.post('http://localhost:8000/admin/login', {
+        $.post(url + 'admin/login', {
             user_name: myname,
             password: mypassword
         }, function(res) {
             // console.log(res);
             callback(res);
         })
+    },
+    logout: function(callback) {
+        $.post(url + 'admin/logout', function(res) {
+            // console.log(res);
+            callback(res)
+        })
+    },
+    infor: function(callback) {
+        $.get(url + 'admin/getuser', function(res) {
+            callback(res)
+        })
+
     }
 }
